207cb3ccddfcc1fda82e03eee30b6e2761e4a2d0
dianahasiuk <diana.hasiuk.23@pnu.edu.ua>
2025-12-05 19:02:04 +0000

    Lab 2: resume and contacts with templates

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return "Hello, world!"
 
-if __name__ == '__main__':
+@app.route("/")
+def resume():
+    # головна сторінка "Резюме"
+    return render_template("resume.html", title="Резюме")
+
+
+@app.route("/contacts", methods=["GET", "POST"])
+def contacts():
+    # сторінка "Контакти" з формою-заглушкою
+    return render_template("contacts.html", title="Контакти")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
